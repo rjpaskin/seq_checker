8ef813392650fd2f9929977e9d6d450619752863
Rob Paskin <rjpaskin+git@gmail.com>
2012-09-04 16:49:10 +0100

    Basic structure for rendering, use erubis

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
 
 gem 'sinatra', :require => 'sinatra/base'
+gem 'erubis'
+
 gem "bio"
--- seq_check.rb
+++ seq_check.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'erubis'
 
 class SeqCheck < Sinatra::Base
   get '/' do
-    
+    erb ''
   end
 end
