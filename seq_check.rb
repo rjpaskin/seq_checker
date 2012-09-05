eea601643338f0381d1a9f27f0c8900dc1a88e38
Rob Paskin <rjpaskin+git@gmail.com>
2012-09-05 23:27:06 +0100

    Displace insertions to below alignment, increase text size of alignments

--- seq_check.rb
+++ seq_check.rb
@@ -6,6 +6,15 @@ module SeqUtils
     char = '-'#'&nbsp;'
     "#{char * (positions.min - 1)}#{seq}#{char * (total_length - positions.max)}"
   end
+  
+  def highlight_query_insertions(query, target)
+    query = query.split(//)
+    target = target.split(//)
+    
+    query.each_with_index do |na, pos|
+      target[pos] = '</span><span class="insertion"><i class="arrow"></i><span>' + target[pos] + '</span></span><span>' if na == '-'
+    end
+    return '<span>' + target.join + '</span>'
   end
 end
 
