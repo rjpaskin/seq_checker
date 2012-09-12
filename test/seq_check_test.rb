e97222f9f35ed8ad7e27a1d5349ce5d6e3ee7810
Rob Paskin <rjpaskin+git@gmail.com>
2012-09-12 11:46:48 +0100

    Set test environment, improve index test

--- test/seq_check_test.rb
+++ test/seq_check_test.rb
@@ -3,6 +3,8 @@ require File.expand_path('../../config/setup.rb', __FILE__)
 require 'test/unit'
 require 'rack/test'
 
+ENV['RACK_ENV'] = 'test'
+
 class SeqCheckTest < Test::Unit::TestCase
   include Rack::Test::Methods
   
@@ -13,5 +15,6 @@ class SeqCheckTest < Test::Unit::TestCase
   def test_index
     get '/'
     assert last_response.ok?
+    assert last_response.content_length > 0
   end
 end
